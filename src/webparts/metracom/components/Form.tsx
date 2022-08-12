69d6950965dea7bba51f07c90a8b2d7d65428498
Gaetanerk <gaetan.turben@outlook.com>
2022-08-12 16:33:46 +0200

    12082022 16:33

--- src/webparts/metracom/components/Form.tsx
+++ src/webparts/metracom/components/Form.tsx
@@ -1,11 +1,37 @@
 import * as React from 'react';
 import { TextField } from '@fluentui/react/lib/TextField';
 import { DefaultButton } from '@fluentui/react/lib/Button';
+import { spfi, SPFx } from "@pnp/sp";
+import "@pnp/sp/webs";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import React, { useState } from 'react'
 
-export function Form() {
+const Form = () => {
+  const [formData, setFormData] = useState({
+    object: "",
+    dateHour: "",
+    orderDay: "",
+    organizer: "",
+    nameProject: "",
+    customer: "",
+    attendees: "",
+    status: ""
+  })
+
+export function Form(props) {
+
+  async function getList() {
+    console.log(props.description);
+    console.log(props.context);
+    const sp = spfi().using(SPFx(props.context));
+    const items = await sp.web.lists.getByTitle("Liste de réunion").items();
+    console.log(items);
+  }
     return (
         <form>
-          <TextField label="Objet" required />
+          <TextField {(e) => setFormData({...formData, object: e.target.value})} value={formData.object} label="Objet" required />
+          <TextField label="Date et heure" required />
           <TextField label="Ordre du jour" required />
           <TextField label="Organisateur" required />
           <TextField label="Nom du projet" required />
@@ -13,7 +39,8 @@ export function Form() {
           <TextField label="Participants" required />
           <TextField label="Etat" required />
           <br />
-          <DefaultButton text="Valider le formulaire" />
+          <DefaultButton text="Valider le formulaire" onClick={getList}/>
         </form>
     )
   };
+
